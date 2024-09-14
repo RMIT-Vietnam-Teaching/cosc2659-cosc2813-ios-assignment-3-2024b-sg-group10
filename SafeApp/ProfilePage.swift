7734f54e83d7836d702630d77865c67c625d18ef
GuitarHero2004 <118866127+GuitarHero2004@users.noreply.github.com>
2024-09-14 11:04:53 +0700

    added dummy avatar

---
+++ SafeApp/ProfilePage.swift
@@ -0,0 +1,18 @@
+//
+//  ProfilePage.swift
+//  SafeApp
+//
+//  Created by Minh Tran Nguyen Anh on 14/9/24.
+//
+
+import SwiftUI
+
+struct ProfilePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfilePage()
+}
