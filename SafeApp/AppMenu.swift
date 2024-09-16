d51922570983084df13ab861e2ce23003b402f7c
GuitarHero2004 <118866127+GuitarHero2004@users.noreply.github.com>
2024-09-16 10:33:37 +0700

    modified the Preview and added some styling

---
+++ SafeApp/AppMenu.swift
@@ -0,0 +1,18 @@
+//
+//  AppMenu.swift
+//  SafeApp
+//
+//  Created by Minh Tran Nguyen Anh on 16/9/24.
+//
+
+import SwiftUI
+
+struct AppMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppMenu()
+}
