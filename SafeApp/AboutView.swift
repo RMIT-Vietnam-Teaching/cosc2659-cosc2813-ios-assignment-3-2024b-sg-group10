87b1729021493145ddc8427848854c5ab90f885f
GuitarHero2004 <118866127+GuitarHero2004@users.noreply.github.com>
2024-09-16 13:11:41 +0700

    edited safety tip with a more detailed description

---
+++ SafeApp/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  SafeApp
+//
+//  Created by Minh Tran Nguyen Anh on 16/9/24.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
